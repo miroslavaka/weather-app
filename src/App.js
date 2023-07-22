ee6d7bfeb8418e507df48ee92863e802a84a7eef
miroslavaka <kacmarova.miroslava@gmail.com>
2023-07-22 12:20:35 +0200

    adding Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Weather from './Weather';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="App-container">
         <h1 className="title">Weather Forecast</h1>
         <Weather />
+        <Footer />
       </div>
     </div>
   );
